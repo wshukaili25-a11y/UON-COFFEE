2c3e57d60121e689be054d7c796064d41b06a20c
wshukaili25-a11y <w.shukaili25@gmail.com>
2026-06-10 03:48:57 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'uon-coffee-v1'
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/features.html',
+  '/tools.html',
+  '/summaries.html',
+  '/groups.html',
+  '/marketplace.html',
+  '/schedule.html',
+  '/admin.html',
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(() => {})
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone()
+        caches.open(CACHE).then(cache => cache.put(e.request, clone))
+        return res
+      })
+      .catch(() => caches.match(e.request))
+  )
+})
