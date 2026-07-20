d4c2508f108f93c659ec902e93266e27c86912f6
wshukaili25-a11y <w.shukaili25@gmail.com>
2026-07-20 17:44:15 +0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const SUPABASE_URL = 'https://fjkxahhoacqlpiafmfyd.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqa3hhaGhvYWNxbHBpYWZtZnlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMTkwMjQsImV4cCI6MjA5Mzg5NTAyNH0.Ls3gaXRso3BgcDAByoYeQQot_KbAOfREvxyW8u4vkGA'
+const SUPABASE_URL = 'https://irkhvydgxpseflggbeqq.supabase.co'
+const SUPABASE_ANON_KEY = 'sb_publishable_gZ9tyM1udrkuQIXHqDtToQ_FyFmePgH'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: true, autoRefreshToken: true },
