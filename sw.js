6de173e5944976579d04dfb52fc299e6860c9beb
wshukaili25-a11y <w.shukaili25@gmail.com>
2026-07-21 17:45:49 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,6 @@
-const CACHE_PREFIX = 'uon-';
-
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(
-      keys
-        .filter(key => key.startsWith(CACHE_PREFIX) || key.includes('uon'))
-        .map(key => caches.delete(key))
-    );
-
-    await self.clients.claim();
-
-    const clients = await self.clients.matchAll({
-      type: 'window',
-      includeUncontrolled: true
-    });
-
-    for (const client of clients) {
-      client.postMessage({ type: 'UON_CACHE_CLEARED' });
-    }
-
-    await self.registration.unregister();
-  })());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil((async()=>{
+ const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));
+ await self.clients.claim();await self.registration.unregister();
+})()));
+self.addEventListener('fetch',event=>event.respondWith(fetch(event.request)));
