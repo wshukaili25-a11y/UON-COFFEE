774f62b608004871a9af283dce6de53236065e9b
wshukaili25-a11y <w.shukaili25@gmail.com>
2026-08-01 01:17:23 +0400

    Load course administration V32.3

--- js/admin-courses-v32.js
+++ js/admin-courses-v32.js
@@ -1,2 +1,2 @@
-// Compatibility loader for cached V32.1 admin pages.
-import './admin-courses-v322.js?v=32.2.1';
+// Compatibility loader for cached course administration pages.
+import './admin-courses-v323.js?v=32.3.0';
