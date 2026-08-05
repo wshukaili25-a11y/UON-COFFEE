74936e6341ceddc996feea4e838c62774b8da0f3
wshukaili25-a11y <w.shukaili25@gmail.com>
2026-08-05 04:14:08 +0400

    Refresh service worker for compact quick tools

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='48.0.3';
+const VERSION='48.0.4';
 const STATIC_CACHE=`uonhub-static-${VERSION}`;
 const PAGE_CACHE=`uonhub-pages-${VERSION}`;
 const DATA_CACHE=`uonhub-data-${VERSION}`;
