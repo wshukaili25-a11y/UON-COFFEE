7743412a5ae3eac6cc7f0ccf9dfcc2c45852857f
wshukaili25-a11y <w.shukaili25@gmail.com>
2026-09-02 17:59:08 +0400

    chore(pwa): bump cache for student suite rollout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '61.1.0';
+const VERSION = '61.2.0';
 const STATIC_CACHE = `uonhub-static-${VERSION}`;
 const PAGE_CACHE = `uonhub-pages-${VERSION}`;
 const DATA_CACHE = `uonhub-data-${VERSION}`;
