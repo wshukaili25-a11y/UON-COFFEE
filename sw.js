a3e8c97d82b2a08aad8ed062cdaa98f2e42b59a6
wshukaili25-a11y <w.shukaili25@gmail.com>
2026-09-02 18:09:15 +0400

    chore(pwa): roll out complete focus suite

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '62.0.0';
+const VERSION = '62.0.1';
 const STATIC_CACHE = `uonhub-static-${VERSION}`;
 const PAGE_CACHE = `uonhub-pages-${VERSION}`;
 const DATA_CACHE = `uonhub-data-${VERSION}`;
