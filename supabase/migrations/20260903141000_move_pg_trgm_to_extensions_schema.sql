a4a28db023317706a234f136a4b6dc7cb74464cd
wshukaili25-a11y <w.shukaili25@gmail.com>
2026-09-03 15:30:09 +0400

    security(db): move pg_trgm out of public schema

---
+++ supabase/migrations/20260903141000_move_pg_trgm_to_extensions_schema.sql
@@ -0,0 +1,3 @@
+alter function public.uon_global_search_v42(text,integer,text) set search_path='public','extensions';
+alter function public.uon_global_search_v44(text,integer,text) set search_path='public','extensions';
+alter extension pg_trgm set schema extensions;
