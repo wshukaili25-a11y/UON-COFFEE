bdb9531c6ec88358a01646e0a4164adc105cea32
wshukaili25-a11y <w.shukaili25@gmail.com>
2026-09-04 21:00:11 +0400

    chore(schedule): bust EduWave transport cache

--- js/schedule-eduwave-v1.js
+++ js/schedule-eduwave-v1.js
@@ -1,2 +1,2 @@
-import './schedule-eduwave-transport-v3.js?v=66.1.0';
-import './schedule-eduwave-v2.js?v=66.1.0';
+import './schedule-eduwave-transport-v3.js?v=66.3.0';
+import './schedule-eduwave-v2.js?v=66.3.0';
