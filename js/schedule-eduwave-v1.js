e892d1953b97d4748377201a2ab1d0a5fb80c01d
wshukaili25-a11y <w.shukaili25@gmail.com>
2026-09-04 21:08:36 +0400

    chore(schedule): publish stable EduWave batch transport

--- js/schedule-eduwave-v1.js
+++ js/schedule-eduwave-v1.js
@@ -1,2 +1,2 @@
-import './schedule-eduwave-transport-v3.js?v=66.3.1';
-import './schedule-eduwave-v2.js?v=66.3.1';
+import './schedule-eduwave-transport-v3.js?v=66.4.0';
+import './schedule-eduwave-v2.js?v=66.4.0';
